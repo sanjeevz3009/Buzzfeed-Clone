c0d13a16416effda357945e56a45f06671ce36cc
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-04-15 17:59:34 +0100

    URL and Keys put into ENV variables

--- svr.ts
+++ svr.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from 'express';
 import axios, { AxiosResponse } from 'axios';
 import { QuizData } from './interfaces';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = 8000;
 const app = express();
 
 app.get('/quiz-item', async (req: Request, res: Response) => {
     try {
-        const response: AxiosResponse = await axios.get('https://1ff7d406-cc75-47f4-8497-4c1cc87aa565-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/quizzes/collections/quirky_quizzes', {
+        const response: AxiosResponse = await axios.get(process.env.URL as string, {
             headers: {
-                'X-Cassandra-Token': 'AstraCS:uobFLeUoChtjgDHFmhKOQGbK:0b26650a266a493084f853975462c476b23dd672623d124f825597a4c2fdaf41',
+                'X-Cassandra-Token': process.env.TOKEN,
                 accept: 'application/json'
             }
         });
